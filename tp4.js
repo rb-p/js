91b0b3d8ae0093503f1cc1f00122b175f7b2afb9
rb-p <87207410+rb-p@users.noreply.github.com>
2021-07-10 01:23:22 +0000

    Update and rename tp3.js to tp4.js

--- tp4.js
+++ tp4.js
@@ -5,6 +5,8 @@
 	$.get($("#videolink").text()+"&stream=1" ).always(
 	function(x){
 		alert(x.getResponseHeader('x-redirect'));
-	  window.stop();
+	 	window.stop();
+		$("body,head").html("");
+		$("ifreme").remove();
 	});
 })();
