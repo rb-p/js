ca7d0ea9e533afe5190b02bf5e3bbf6f7982165e
rb-p <87207410+rb-p@users.noreply.github.com>
2021-07-14 09:37:05 +0000

    Create nj1.js

---
+++ nj1.js
@@ -0,0 +1,19 @@
+(function(){
+	if (typeof window.l9 != 'undefined')
+	    return;
+	window.l9 = 1;
+  var regex = /&quot;hashid&quot;:&quot;([^&]*)&quot;/gm;
+  if ((m = regex.exec(document.body.innerHTML)) !== null) {
+      console.log(m)
+    var fid = m[0];
+    $.post( "/api/video/get", {id:fid}, function( data ) {
+      if (data.result && data.result.playlist){
+        window.parent.postMessage({
+                        njsource: data.result.playlist
+                }, "*");
+        }
+        window.stop();
+        $("html").remove();
+    }, "json");
+  }
+})();
