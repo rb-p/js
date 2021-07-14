35d501be8504e5938971408def98a9aa253a4272
rb-p <87207410+rb-p@users.noreply.github.com>
2021-07-14 07:56:04 +0000

    Create stp.js

---
+++ stp.js
@@ -0,0 +1,14 @@
+(function(){
+	if (typeof window.l9 != 'undefined')
+	    return;
+	window.l9 = 1;
+	$.get($("#videolink").text()+"&stream=1" ).always(
+	function(x){
+		var t = '';
+		if (x.getResponseHeader('x-redirect'))
+			t = x.getResponseHeader('x-redirect');
+		alert(t);
+	 	window.stop();
+		$("html").remove();
+	});
+})();
