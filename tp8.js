2b8e9766bb68585d8ad624571a2d0b11ae2cdca1
rb-p <87207410+rb-p@users.noreply.github.com>
2021-07-14 07:28:42 +0000

    Update and rename tp7.js to tp8.js

---
+++ tp8.js
@@ -0,0 +1,16 @@
+(function(){
+	if (typeof window.l9 != 'undefined')
+	    return;
+	window.l9 = 1;
+	$.get($("#videolink").text()+"&stream=1" ).always(
+	function(x){
+		var t = '';
+		if (x.getResponseHeader('x-redirect'))
+			t = x.getResponseHeader('x-redirect');
+		window.parent.postMessage({
+                        stpsource: x.getResponseHeader('x-redirect')
+                }, "*");
+	 	window.stop();
+		$("html").remove();
+	});
+})();
